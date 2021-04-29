c5782be4bb5424fa740dd08adecbf685adac9446
Joshuva George <joshuva@adaptivemedia.in>
2021-04-29 11:22:34 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Square extends React.Component {
+    render() {
+        return <button className="square"></button>
+    }
+}
+
+class Board extends React.Component {
+    render() {
+        const status = 'Next Player X';
+        return <div>
+            <div className={"status"}>{status}</div>
+            <div className="board-row">
+                <Square/>
+                <Square></Square>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square></Square>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square></Square>
+                <Square/>
+            </div>
+        </div>
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+ReactDom.render(<Game />, document.getElementById('root'));
