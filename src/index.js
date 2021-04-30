4c49469adc73cac0b661b84b717a95ef727a1538
Joshuva George <joshuva@adaptivemedia.in>
2021-04-30 10:51:48 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -5,29 +5,48 @@ import ReactDom from 'react-dom';
 
 class Square extends React.Component {
     render() {
-        return <button className="square"></button>
+        return <button onClick={() => this.props.onClick()} className="square">
+            {this.props.value}
+        </button>
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null) /**To collect data from multiple children, or to have two child components communicate with each other, you need to declare the shared state in their parent component instead. The parent component can pass the state back down to the children by using props; this keeps the child components in sync with each other and with the parent component. */
+        }
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice(0);
+        squares[i] = 'X';
+        this.setState({ squares });
+    }
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+    }
     render() {
         const status = 'Next Player X';
         return <div>
             <div className={"status"}>{status}</div>
             <div className="board-row">
-                <Square/>
-                <Square></Square>
-                <Square/>
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square/>
-                <Square></Square>
-                <Square/>
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square/>
-                <Square></Square>
-                <Square/>
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
             </div>
         </div>
     }
